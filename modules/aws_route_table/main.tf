32aa7c425cdd39dd4776710af78541e2f044bb87
Dinush Chathurya <dinushchathurya21@gmail.com>
2022-10-14 19:13:23 +0530

    feat: Add route table module

---
+++ modules/aws_route_table/main.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table" "example" {
+    vpc_id = var.vpc_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = var.internet_gateway_id
+    }
+    tags   = var.tags
+}
+
+
---
+++ modules/aws_route_table/variable.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+
+}
+
+variable "internet_gateway_id" {
+
+}
+
+variable "tags" {
+
+}
